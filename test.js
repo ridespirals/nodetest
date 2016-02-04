96f058a2ac6b973bd91678b606b8ef7b1fff1dc1
John Varga <john.g.varga@gmail.com>
2016-02-03 20:23:44 -0800

    tabs -> spaces

--- test.js
+++ test.js
@@ -2,22 +2,22 @@ var prompt = require('prompt');
 var chalk = require('chalk');
 
 var schema = {
-	properties: {
-		name: {
-			pattern: /^[a-zA-Z\s\-]+$/,
-			message: 'Name must only be letters, spaces, and dashes',
-			required: true
-		},
-		password: {
-			hidden: true
-		}
-	}
+    properties: {
+        name: {
+            pattern: /^[a-zA-Z\s\-]+$/,
+            message: 'Name must only be letters, spaces, and dashes',
+            required: true
+        },
+        password: {
+            hidden: true
+        }
+    }
 };
 
 prompt.start();
 
 prompt.get(schema, function(err, result) {
-	console.log('Command-line input received:');
-	console.log(chalk.bold('	name:'), chalk.cyan(result.name));
-	console.log(chalk.bold('	password:'), chalk.red(result.password));
+    console.log('Command-line input received:');
+    console.log(chalk.bold('    name:'), chalk.cyan(result.name));
+    console.log(chalk.bold('    password:'), chalk.red(result.password));
 });
