80663a885e0ebcc30f032eefd34d7786a9360043
John Varga <john.g.varga@gmail.com>
2016-02-05 19:38:54 -0800

    parse dwarf stories

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 var prompt = require('prompt');
 var chalk = require('chalk');
+var yaml = require('js-yaml');
+var fs = require('fs');
 
 var schema = {
     properties: {
@@ -14,10 +16,24 @@ var schema = {
     }
 };
 
-prompt.start();
+// prompt.start();
 
-prompt.get(schema, function(err, result) {
-    console.log('Command-line input received:');
-    console.log(chalk.bold('    name:'), chalk.cyan(result.name));
-    console.log(chalk.bold('    password:'), chalk.red(result.password));
-});
+// prompt.get(schema, function(err, result) {
+//     console.log('Command-line input received:');
+//     console.log(chalk.bold('    name:'), chalk.cyan(result.name));
+//     console.log(chalk.bold('    password:'), chalk.red(result.password));
+// });
+
+try {
+    var doc = yaml.safeLoad(fs.readFileSync('/Users/johnvarga/projects/ridespirals.com/grunt-version/src/data/dwarves_two.yml'));
+    //console.log(doc);
+
+    console.log(chalk.green(doc.story_title));
+    for(var x in doc.story_parts) {
+        var part = doc.story_parts[x];
+        console.log('  ', chalk.bold.yellow(part.name), '\t', chalk.cyan(part.ascii));
+    }
+
+} catch(e) {
+    console.log(chalk.bold.red('Error: ', e));
+}
